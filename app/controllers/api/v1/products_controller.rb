4af4bc1436f4a6050027caf5f3546ef731cc58db
esilvac <esilva1995@gmail.com>
2017-07-15 01:05:31 -0500

    Solucion Reto2

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,9 +1,22 @@
-module Api
-    module V1
-        class ProductsController < ApplicationController
-          def index
-            @products = Product.all
-          end
-        end
+class Api::V1::ProductsController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    render json: Product.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render json: @product, status: 201
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+
+  end
+
+  private 
+    def product_params
+      params.require(:product).permit(:name, :price)
     end
 end
