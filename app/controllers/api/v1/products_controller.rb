c05f000aa292d2a34beb419fea7791e3bc28508c
esilvac <esilva1995@gmail.com>
2017-07-15 22:58:24 -0500

    Solucion reto3 rails

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -12,7 +12,21 @@ class Api::V1::ProductsController < ApplicationController
     else
       render json: @product.errors, status: :unprocessable_entity
     end
+  end
 
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      render json: @product, status: 200
+    else
+      render json: @product.errors, status: 422
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    render json: @product, status: 204
   end
 
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :products
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :products, only: [:create, :index]
+      resources :products, except: [:show, :edit]
     end
   end
 end
